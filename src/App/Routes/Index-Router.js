543489d3daa99a1f65d7eb4f2c31d15bb24545ce
Edu578 <eduardo.arevalo072@gmail.com>
2022-02-15 21:05:08 -0300

    se crea un componente de logueo

--- src/App/Routes/Index-Router.js
+++ src/App/Routes/Index-Router.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from '../components/Acreditar/Login/Login';
 import Page from '../Page/page';
 
 export function App() {
   return (
     <BrowserRouter>
+    <Login/>
       <Routes>
         <Route path="/" element={<Page />} />
       </Routes>
---
+++ src/App/components/Acreditar/Login/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const Login = () => {
+  return <div>
+
+    <div>
+        <h2> Logotipo</h2>
+    </div>
+  </div>
+};
+
+export default Login;
