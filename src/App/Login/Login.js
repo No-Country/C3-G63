47329e769a436af3b9492f75777d1f11e3ead5d0
Edu578 <eduardo.arevalo072@gmail.com>
2022-02-16 20:13:49 -0300

    se crea carpeta assets

--- src/App/Login/Login.js
+++ src/App/Login/Login.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 
 
 const Login = () => {
+
+
+
   return <div>
 
     <div>
-        <h2> Logotipo</h2>
+        <h2>Logo</h2>
+    </div>
+    <div>
+      <h1>Ingresa</h1>
+    </div>
+    <div>
+      <button>Login</button><br/>
+      <button>Sign in</button>
     </div>
   </div>
 };
