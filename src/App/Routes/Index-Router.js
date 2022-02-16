f71bffe7fa42b4ec6c17b61d9b9b75ba43d92768
Edu578 <eduardo.arevalo072@gmail.com>
2022-02-15 21:17:04 -0300

    se agrega imagen a carpeta

--- src/App/Routes/Index-Router.js
+++ src/App/Routes/Index-Router.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Login from '../components/Acreditar/Login/Login';
+import Login from '../Login/Login';
 import Page from '../Page/page';
 
 export function App() {
