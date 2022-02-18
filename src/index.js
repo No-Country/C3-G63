6f87b42b83a2acc51c52435cd5d82e5c4438880d
malks14 <73920001+malks14@users.noreply.github.com>
2022-02-18 17:30:17 -0300

    feat: wallet

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from "./App";
+import IndexRouter from './App/Routes/Index-Router';
 import "./index.css"
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <IndexRouter />
   </React.StrictMode>,
   document.getElementById('root')
 );
