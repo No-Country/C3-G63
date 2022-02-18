f8490a1e2db02d777685ff8f6c17ff3911d3f54c
ArleysGatica <gatica.arleys16@gmail.com>
2022-02-18 09:52:40 -0600

    Error en el page... ya resulto

--- src/App/Login/Login.jsx
+++ src/App/Login/Login.jsx
@@ -1,26 +1,18 @@
 import React from 'react';
-import  './login.scss'
+import './login.scss'
 
-
-
-
-
-
-
-const Login = () => {
-  return <div>
-    <div className='prueba'>
-      <h1>Esto es un login</h1>
-    </div>
-    <div>
-      <h1>LOGIN</h1>
-    </div>
-    <button>Login with Google</button>
-    <button>Login with Facebook</button>
-    <button>Login with mail</button>
-    
-    
-  </div>
+export const Login = () => {
+  return (
+    <>
+      <div className='Container-main'>
+        <h1>Esto es un login</h1>
+      </div>
+      <div>
+        <h1>LOGIN</h1>
+      </div>
+      <button>Login with Google</button>
+      <button>Login with Facebook</button>
+      <button>Login with mail</button>
+    </>
+  )
 };
-
-export default Login;
--- src/App/Routes/Index-Router.js
+++ src/App/Routes/Index-Router.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import SignUp from '../components/SignUp/SignUp';
 import FormLogin from '../Login/formLogin';
 import Ingresa from '../Login/Ingresa';
@@ -8,18 +8,18 @@ import Page from '../Page/page';
 export function App() {
   return (
     <BrowserRouter>
-    <Ingresa />
-    <Switch>
+      <Ingresa />
+
       <Routes>
         <Route path="/" element={<Page />} />
       </Routes>
       <Routes>
-        <Route exact path="/formLogin" element={<FormLogin/>} />
+        <Route exact path="/formLogin" element={<FormLogin />} />
       </Routes>
       <Routes>
-        <Route exact path ="/Registro" element={<SignUp/>} />
+        <Route exact path="/Registro" element={<SignUp />} />
       </Routes>
-    </Switch>
+
     </BrowserRouter>
   );
 }
