49afcf06c5dfa51d7e6adbf9bb6cfab29b0fc774
TomasKleinschuster <kleinschustertomas@gmail.com>
2022-02-21 23:09:03 -0300

    Header modifications

--- src/App/components/Header/Header.jsx
+++ src/App/components/Header/Header.jsx
@@ -1,60 +1,57 @@
-import React from 'react';
-import './Header.scss';
-import { faCoins , faSuitcase , faMagnifyingGlass , faArrowRightFromBracket , faUser } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Header.scss";
 
 const Header = () => {
-    return (
-        <div className="header" id="header">
-            <nav  className="nav container">
-                <a className="nav__logo">
-                    <img src="" alt="" />LOGO
-                </a>
+  return (
+    <div className="header" id="header">
+      <nav className="nav container">
+        <a className="nav__logo">
+          <img src="Crypt-logo.png" alt="Logo Criptomoneda" />
+          LOGO
+        </a>
 
-                <div className="nav__menu" id="nav-menu">
-                    <ul className="nav__list">
+        <div className="nav__menu" id="nav-menu">
+          <ul className="nav__list">
+            <li className="nav__item">
+              <a className="nav__link">
+                <i className="fas fa-coins"></i>
+                <span className="nav__name">Coins</span>
+              </a>
+            </li>
 
-                        <li className="nav__item">
-                            <a className="nav__link">
-                                <FontAwesomeIcon className='nav__icon' icon={faCoins} />
-                                <span className="nav__name">Coins</span>
-                            </a>
-                        </li>
+            <li className="nav__item">
+              <a className="nav__link">
+                <i className="fas fa-suitcase"></i>
+                <span className="nav__name">Portfolio</span>
+              </a>
+            </li>
 
-                        <li className="nav__item">
-                            <a className="nav__link">
-                                <FontAwesomeIcon className='nav__icon' icon={faSuitcase} />
-                                <span className="nav__name">Portfolio</span>
-                            </a>
-                        </li>
+            <li className="nav__item">
+              <a className="nav__link">
+                <i className="fas fa-search"></i>
+                <span className="nav__name">Search</span>
+              </a>
+            </li>
 
-                        <li className="nav__item">
-                            <a className="nav__link">
-                                <FontAwesomeIcon className='nav__icon' icon={faMagnifyingGlass} />
-                                <span className="nav__name">Search</span>
-                            </a>
-                        </li>
-
-                        <li className="nav__item">
-                            <a className="nav__link">
-                                <FontAwesomeIcon className='nav__icon' icon={faArrowRightFromBracket} />
-                                <span className="nav__name">Exit </span>
-                            </a>
-                        </li>
-
-                    </ul>
-                </div>
-
-                <div className="nav__item">
-                    <a href="#login" className="nav__link">
-                        <FontAwesomeIcon className='nav__icon' icon={faUser} />
-                        <span className="nav__name">Login </span>
-                    </a>
-                </div>
-
-            </nav>
+            <li className="nav__item">
+              <a className="nav__link">
+                <i className="fas fa-sign-out-alt"></i>
+                <span className="nav__name">Exit </span>
+              </a>
+            </li>
+          </ul>
         </div>
-    )
-}
 
-export default Header
+        <div className="nav__item">
+          <Link to="/login" href="#login" className="nav__link">
+            <i className="fas fa-user"></i>
+            <span className="nav__name">Login </span>
+          </Link>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
