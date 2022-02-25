7d252666872910be4c6de387fadc9ad3f62ed837
malks14 <73920001+malks14@users.noreply.github.com>
2022-02-25 19:18:48 -0300

    docs: map to cards

--- src/Components/UI/Cards.js
+++ src/Components/UI/Cards.js
@@ -6,34 +6,26 @@ const Cards = ({ data }) => {
 
   return (
     <div className={styles.container}>
-      <div className={styles.cardCtn}>
-        <div className={styles.cryptoCtn}>
-          <i className="fab fa-bitcoin fa-2x"></i>
-          <p>Bitcoin</p>
-        </div>
-          <p>$41.000</p>
-        <div>
-          <p>grafico critpo</p>
-        </div>
-      </div>
-      <div className={styles.btnCtn}>
-        <button>Comprar</button>
-        <button>Vender </button>
-      </div>
-      <div className={styles.cardCtn}>
-        <div className={styles.cryptoCtn}>
-          <i className="fab fa-ethereum fa-2x"></i>
-          <p>Ethereum</p>
-        </div>
-          <p>$2.950</p>
-        <div>
-          <p>grafico critpo</p>
-        </div>
-      </div>
-      <div className={styles.btnCtn}>
-        <button>Comprar</button>
-        <button>Vender </button>
-      </div>
+      {data.map((el) => {
+        return (
+          <>
+            <div key={el.name} className={styles.cardCtn}>
+              <div className={styles.cryptoCtn}>
+                <img src={el.logo} />
+                <p>{el.name}</p>
+              </div>
+              <p>{el.price}</p>
+              <div>
+                <p>grafico critpo</p>
+              </div>
+            </div>
+            <div className={styles.btnCtn}>
+              <button>Comprar</button>
+              <button>Vender </button>
+            </div>
+          </>
+        );
+      })}
     </div>
   );
 };
