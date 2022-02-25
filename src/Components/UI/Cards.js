8d210ff914e239f70f08225f3329ff5b511705fa
malks14 <73920001+malks14@users.noreply.github.com>
2022-02-25 20:12:00 -0300

    Portfolio cryptos (#9)
    * docs: new files
    
    * feat: wallet
    
    * docs:  new url
    
    * docs: no api
    
    * docs: fetch petition
    
    * style: logo crypto
    
    * docs: base url
    
    * docs: map to cards
    
    * docs: added layout and wallet
    
    * docs: route name

--- src/Components/UI/Cards.js
+++ src/Components/UI/Cards.js
@@ -2,38 +2,30 @@ import React from "react";
 import styles from "./Cards.module.css";
 
 const Cards = ({ data }) => {
-  console.log(data);
+ 
 
   return (
     <div className={styles.container}>
-      <div className={styles.cardCtn}>
-        <div className={styles.cryptoCtn}>
-          <i className="fab fa-bitcoin fa-2x"></i>
-          <p>Bitcoin</p>
-        </div>
-          <p>$41.000</p>
-        <div>
-          <p>grafico critpo</p>
-        </div>
-      </div>
-      <div className={styles.btnCtn}>
-        <button>Comprar</button>
-        <button>Vender </button>
-      </div>
-      <div className={styles.cardCtn}>
-        <div className={styles.cryptoCtn}>
-          <i className="fab fa-ethereum fa-2x"></i>
-          <p>Ethereum</p>
-        </div>
-          <p>$2.950</p>
-        <div>
-          <p>grafico critpo</p>
-        </div>
-      </div>
-      <div className={styles.btnCtn}>
-        <button>Comprar</button>
-        <button>Vender </button>
-      </div>
+      {data.map((el) => {
+        return (
+          <>
+            <div className={styles.cardCtn}>
+              <div key={el.name} className={styles.cryptoCtn}>
+                <img src={el.logo} />
+                <p>{el.name}</p>
+              </div>
+              <p className={styles.price}>${el.price}</p>
+              <div>
+                <p>grafico critpo</p>
+              </div>
+            </div>
+            <div className={styles.btnCtn}>
+              <button className={styles.btnPrimary}>Comprar</button>
+              <button className={styles.btnSecondary}>Vender </button>
+            </div>
+          </>
+        );
+      })}
     </div>
   );
 };
--- src/Pages/Wallet/WalletPage.js
+++ src/Pages/Wallet/WalletPage.js
@@ -1,23 +1,33 @@
 import React, {useEffect, useState} from 'react';
-import axios from "../../Components/api/apibinance"
+import Header from '../../App/components/Header/Header';
+import axios from "../../Components/api/apicrypto"
 import Cards from '../../Components/UI/Cards';
 import styles from "./WalletPage.module.css"
 
 const WalletPage = () => {
-    const [data, setData] = useState({});
+    const [data, setData] = useState([]);
 
     useEffect(() => {
         const fetchData = async () => {
-            const fetchedData = await axios.get("/v3/ticker/price");
-            console.log(fetchedData)
-            setData(fetchedData);
+            const fetchedData = await axios.get("https://crypto-app-b0955-default-rtdb.firebaseio.com/crypto.json");
+            
+            const loadedCrypto = [];
+
+            for (const key in fetchedData.data) {
+                loadedCrypto.push({
+                    name: fetchedData.data[key].name,
+                    logo: fetchedData.data[key].logo,
+                    price: fetchedData.data[key].price,
+                })
+            }
+            setData(loadedCrypto);
+            
         }
         fetchData()
     }, [])
 
     return (
         <>
-        <div className={styles.headerTest}>HEADER</div>
         <div className={styles.walletCtn}>
             <Cards data={data}/>
         </div>
