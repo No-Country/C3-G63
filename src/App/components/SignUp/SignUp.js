26435cc77b44daad952d727204448c7b45f7bb9a
Edu578 <eduardo.arevalo072@gmail.com>
2022-02-25 13:40:53 -0300

    se finaliza con signUp con facebook y google

--- src/App/components/SignUp/SignUp.js
+++ src/App/components/SignUp/SignUp.js
@@ -2,16 +2,60 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import logo from '../../../Assets/Vector.png'
 import './SignUp.scss'
+import FacebookLogin from 'react-facebook-login';
+import GoogleLogin from "react-google-login";
 
 const SignUp = () => {
+
+
+
+  const responseGoogle = (response) => {
+    console.log(response);
+  }
+
+  const responseFacebook = (response) => {
+    console.log(response);
+  }
+
+  const componentClicked = () =>{
+    console.log("Estas logueado")
+  }
+
   return (
-    <div className='container logoBox'>
-        <div className='logoIcon'>
-            <img src={logo} alt="logo"/>
-        </div>
-        <div className='logo_box_info'>
-            <button >Sign In with Google</button><br/>
-            <button>Sign In with Facebook</button><br/>
+    <div className="container logoBox">
+          <div className="logoIcon">
+              <img src={logo} alt="logo"/>
+          </div>
+          <div className="logo_name">
+              <h1>Login</h1>
+          </div>
+          <div className="logo_box_info">
+            
+              <GoogleLogin 
+              clientId="990173810108-o8a32tut813k0ulff95c1gldnk17occc.apps.googleusercontent.com"
+              render={renderProps => (
+                <button onClick={renderProps.onClick} disabled={renderProps.disabled}>Login with Google</button>
+              )}
+              onSuccess={responseGoogle}
+              onFailure={responseGoogle}
+              cookiePolicy={'single_host_origin'}
+              
+              
+              
+              />
+            <br/> 
+            
+            <FacebookLogin 
+              cssClass="facebook"
+              appId="538754370702621"
+              autoLoad={false}
+              fields="name,email,picture"
+              onClick={componentClicked}
+              callback={responseFacebook}
+            
+            />
+              
+            <br/>
             <Link to={`/registroEmail`}> <button>Sign In with Email</button></Link> 
         </div>
 
