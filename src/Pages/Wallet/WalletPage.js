18dae95b2841ef1e5c6380ac47606f1f78a33602
malks14 <73920001+malks14@users.noreply.github.com>
2022-02-25 19:17:40 -0300

    docs: fetch petition

--- src/Pages/Wallet/WalletPage.js
+++ src/Pages/Wallet/WalletPage.js
@@ -1,16 +1,26 @@
 import React, {useEffect, useState} from 'react';
-import axios from "../../Components/api/apibinance"
+import axios from "../../Components/api/apicrypto"
 import Cards from '../../Components/UI/Cards';
 import styles from "./WalletPage.module.css"
 
 const WalletPage = () => {
-    const [data, setData] = useState({});
+    const [data, setData] = useState([]);
 
     useEffect(() => {
         const fetchData = async () => {
-            const fetchedData = await axios.get("/v3/ticker/price");
+            const fetchedData = await axios.get("https://crypto-app-b0955-default-rtdb.firebaseio.com/crypto.json");
             console.log(fetchedData)
-            setData(fetchedData);
+            const loadedCrypto = [];
+
+            for (const key in fetchedData.data) {
+                loadedCrypto.push({
+                    name: fetchedData.data[key].name,
+                    logo: fetchedData.data[key].logo,
+                    price: fetchedData.data[key].price,
+                })
+            }
+            setData(loadedCrypto);
+            console.log(loadedCrypto)
         }
         fetchData()
     }, [])
