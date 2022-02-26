196aa10fc3757052f15f1893978da521bd9a97d3
malks14 <73920001+malks14@users.noreply.github.com>
2022-02-26 17:30:26 -0300

    docs: signup logic

--- src/App/components/SignUp/FormEmail.js
+++ src/App/components/SignUp/FormEmail.js
@@ -1,17 +1,89 @@
-import React from 'react'
-import './SignUp.scss'
+import React, { useState } from "react";
+import { useNavigate } from "react-router";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import LoadingSpinner from "../../../Components/UI/LoadingSpinner";
+import { auth } from "../../../firebase";
+import "./SignUp.scss";
+import useInput from "../../../hooks/use-input";
 
 const FormEmail = () => {
-  return (
-    <div className="formCtn">
-        <form className='formulario'>
-                <label htmlFor="">Usuario</label>
-                <input type="text" placeholder="Usuario" id='usuario'/>
-                <input type="passoword" placeholder="Contraseña"/>
-                <button>Confirmar</button>
-            </form>
-    </div>
-  )
-}
+  const isNotEmpty = value => value.trim() !== '' && value.length > 5;
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
 
-export default FormEmail
+  const {
+    value: enteredEmail, 
+    isValid: enteredEmailIsValid,
+    hasError: emailInputHasError, 
+    valueChangeHanlder: emailChangedHandler, 
+    inputBlurHandler: emailBlurHandler,
+
+  } = useInput(value => value.includes('@'));
+
+  const {
+    value: enteredPassword, 
+    isValid: enteredPasswordIsValid,
+    hasError: passwordInputHasError, 
+    valueChangeHanlder: passwordChangedHandler, 
+    inputBlurHandler: passwordBlurHandler,
+
+  } = useInput(isNotEmpty);
+
+  let formIsValid = false;
+
+  if (enteredEmailIsValid && enteredPasswordIsValid) {
+    formIsValid = true;
+  } 
+
+  const registerHandler = async (event) => {
+    setIsLoading(true)
+    event.preventDefault();
+    try {
+      const user = await createUserWithEmailAndPassword(
+        auth,
+        enteredEmail,
+        enteredPassword
+      );
+      setIsLoading(false)
+      navigate("/");
+      console.log(user);
+    } catch (error) {
+      setIsLoading(false)
+ 
+    }
+  };
+
+  return (
+    <>
+    <div className="formCtn">
+      <form className="formulario" onSubmit={registerHandler}>
+        <label htmlFor="user">Email</label>
+        <input
+          id="user"
+          type="text"
+          placeholder="Email"
+          onBlur={emailBlurHandler}
+          onChange={emailChangedHandler}
+          value={enteredEmail}
+        />
+        {emailInputHasError && <p className="error">Por favor poner un email valido</p>}
+        <label htmlFor="password">Contraseña</label>
+        <input
+          id="password"
+          type="password"
+          placeholder="Contraseña"
+          onChange={passwordChangedHandler}
+          onBlur={passwordBlurHandler}
+          value={enteredPassword}
+        />
+        {passwordInputHasError && <p className="error">La contraseña debe ser de 6 caracteres</p>}
+        <button disabled={!formIsValid}>Confirmar</button>
+      </form>
+      {isLoading && <LoadingSpinner />}
+    </div>
+    </>
+  );
+};
+
+export default FormEmail;
