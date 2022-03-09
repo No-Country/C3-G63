4ec668299164f723c0e845b740e09ae63582e710
malks14 <73920001+malks14@users.noreply.github.com>
2022-03-09 18:55:19 -0300

    Acreditar component (#27)
    * name
    
    * name
    
    * first commit

--- src/App/components/Acreditar/acreditar.js
+++ src/App/components/Acreditar/acreditar.js
@@ -1,36 +1,18 @@
-import React from 'react';
-import { AcreditarView } from './Acreditar';
-import swal from 'sweetalert';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import styles from "./Acreditar.module.css";
 
-export const Acreditar = () => {
-
-  const [acreditar, setAcreditar] = useState(false);
-  const [configuracion, setConfiguracion] = useState(false);
-  const [logo, setLogo] = useState(0);
-  const [name, setName] = useState('');
-
-  const alertClick = () => {
-    swal({
-      title: "¿Estas seguro?",
-      text: "Una vez acreditado no podras revertir el cambio",
-      icon: "warning",
-      buttons: ["NO", "Aceptar"],
-      dangerMode: true,
-    })
-  }
+const Acreditar = () => {
+  const navigate = useNavigate();
 
   return (
-    <AcreditarView
-      acreditar={acreditar}
-      configuracion={configuracion}
-      logo={logo}
-      name={name}
-      setAcreditar={setAcreditar}
-      setConfiguracion={setConfiguracion}
-      setLogo={setLogo}
-      setNome={setName}
-      list={list}
-      alertClick={alertClick}
-    />
+    <div className={styles.acreditarCtn}>
+      <div className={styles.acreditarCtn__info}>
+        <h2>Pay with:</h2>
+        <button onClick={() => navigate("/checkout")}>Credit/Debit Card</button>
+      </div>
+    </div>
   );
 };
+
+export default Acreditar;
